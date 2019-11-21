17c14e995e3286ffcddbcba83e8b661e9dcbe559
Jisoo Shin <jisooshin@Jisoos-MacBook-Pro-2.local>
2019-11-21 11:07:49 -0500

    c

--- functions.js
+++ functions.js
@@ -1,29 +1,41 @@
-// var f = document.getElementById("featured");
-// var menu = document.getElementsByClassName("menu");
-// var list = document.getElementsByClassName("list");
-//
-// for (var i = 0; i < f.length; i++) {
-//   f[i].onclick = function(event) {
-//     this.classList.toggle('expanded');
-//   }
-// }
-
-// function myFunction(e) {
-//   var elems = document.querySelectorAll(".active");
-//   [].forEach.call(elems, function(el) {
-//     el.classList.remove("active");
-//   });
-//   e.target.className = "active";
-// }
-
-var balls = document.getElementsByClassName("ball");
+var eyeball = document.getElementsByClassName("eyeball");
 document.onmousemove = function(){
   var x = event.clientX * 100 / window.innerWidth + "%";
   var y = event.clientY * 100 / window.innerHeight + "%";
 
   for(var i=0;i<2;i++){
-    balls[i].style.left = x;
-    balls[i].style.top = y;
-    balls[i].style.transform = "translate(-"+x+", -"+y+")";
+    eyeball[i].style.left = x;
+    eyeball[i].style.top = y;
+    eyeball[i].style.transform = "translate(-"+x+", -"+y+")";
   }
+}
+
+// var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true,
+// });
+//
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity( '.main-carousel', {
+//   // options
+//   autoPlay: true,
+//   autoPlay: 1500
+// });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 1500); // Change image every 2 seconds
 }
