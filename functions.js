480420073f79bc465c1379bf8b07fe0ce6a2a9ff
Jisoo Shin <jisooshin@Jisoos-MBP-2.fios-router.home>
2020-04-26 17:59:25 -0400

    iland, italy-bridge edge fix

--- functions.js
+++ functions.js
@@ -128,12 +128,12 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("filterby");
-var btns = btnContainer.getElementsByClassName("filter");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active","");
-    this.className += " active";
-  });
-}
+// var btnContainer = document.getElementById("filterby");
+// var btns = btnContainer.getElementsByClassName("filter");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active","");
+//     this.className += " active";
+//   });
+// }
