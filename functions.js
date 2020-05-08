da6d194800a012ad8db65c5fd0fc08e2373509fd
Jisoo Shin <jisooshin@Jisoos-MBP-2.fios-router.home>
2020-05-08 18:43:24 -0400

    huge update

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+function goBack() {
+  window.history.back();
+}
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
