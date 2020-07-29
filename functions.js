af239d37ea3c0ce941223967a2bc6d770fb2f30d
Jisoo Shin <jisooshin@Jisoos-MacBook-Pro-2.local>
2020-07-29 10:55:28 -0400

    delete

--- functions.js
+++ functions.js
@@ -6,56 +6,11 @@ function goBack() {
   window.history.back();
 }
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.querySelector('.menubar').style.top = "0";
-//   } else {
-//     document.querySelector('.menubar').style.top = "-8rem";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector('#totop').style.bottom = "-7vw";
-  } else {
-    document.querySelector('#totop').style.bottom = "7vw";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
 function toggleMenu() {
   document.querySelector('.menu').classList.toggle("showMenu");
   document.querySelector('body').classList.toggle("stopscroll");
   document.querySelector('#menu').classList.toggle("selected");
 };
-
-var process = document.querySelector('.process');
-var overlay = document.querySelector('.overlay')
-var body = document.querySelector('body')
-
-function showProcess() {
-    process.classList.add("open");
-    overlay.classList.add("showOverlay");
-    body.classList.add("stopscroll");
-};
-
-window.addEventListener('mouseup', function(event){
-  if(event.target != process && event.target.parentNode != process){
-    process.classList.remove("open"); overlay.classList.remove("showOverlay");
-    body.classList.remove("stopscroll");
-  }
-});
-
-function hideProcess() {
-  process.classList.remove("open"); overlay.classList.remove("showOverlay");
-  body.classList.remove("stopscroll");
-}
 
 function copyEmail() {
   /* Get the text field */
